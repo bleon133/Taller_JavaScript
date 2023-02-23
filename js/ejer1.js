340d46aafda4d1b6a70714beda5acb20daac9597
BrayanLeon <brot10102017@gmail.com>
2023-02-23 10:03:17 -0500

    3version

--- js/ejer1.js
+++ js/ejer1.js
@@ -8,13 +8,13 @@ const visualizar = document.getElementById('verAnimales')
 const cantExpAnimales = document.getElementById('verCantidadAnimales')
 const buscar = document.getElementById('buscar')
 //Base de datos
-const animales = ['Vaca','Oveja','Perro','Gato','Araña','Ave','Pez']
+const animales = ['vaca','oveja','perro','gato','araña','ave','pez']
 var num = animales.length+1
 
 //Funcionalidades
 document.getElementById('number').innerHTML=`${num}. `
 agregar.addEventListener('click',()=>{
-    let name = animal.value
+    let name = animal.value.toLowerCase()
     if(num <= 20){
         if(name == ''){
             alert('Error: No ha digitado ningún nombre.')
@@ -38,14 +38,23 @@ visualizar.addEventListener('click',()=>{
     document.getElementById('pantalla').innerHTML=contenido
 })
 cantExpAnimales.addEventListener('click',()=>{
-    let contenido = ''
-    contenido += `<table><th>Num</th><th>Animales</th>`
-    animales.forEach((indAnimal,index)=>{
-        console.log('prueba')
-        if(index >= minimo){ 
-            contenido += `<tr><td>${index+1}</td><td>${indAnimal}</td></tr>`
-        }   
-    })
-    contenido += `</table>`
-    document.getElementById('pantalla').innerHTML=contenido
+    if(minimo.value != ''){
+        let contenido = ''
+        contenido += `<table><th>Animales</th>`
+        let filtradoAnimales = animales.slice(minimo.value-1, animales.length)
+        filtradoAnimales.forEach((indAnimal)=>{
+            contenido += `<tr><td>${indAnimal}</td></tr>`
+        })
+        contenido += `</table>`
+        document.getElementById('pantalla').innerHTML=contenido
+    }else{
+        alert('Error: No ha digitado un valor minimo.')
+    }
+})
+buscar.addEventListener('click',()=>{
+    if(animales.includes(buscador.value.toLowerCase())){
+        document.getElementById('pantalla').innerHTML=`El animal ${buscador.value} existe.`
+    }else{
+        document.getElementById('pantalla').innerHTML=`El animal ${buscador.value} NO existe.`
+    }
 })
